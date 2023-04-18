70873b6b479f6649ff66bab07765595b766e3f7b
Alfonso Lin <user.name>
2023-04-18 22:25:21 +0800

    Update HW3 part4

--- HW3/AI_HW3/run.sh
+++ HW3/AI_HW3/run.sh
@@ -6,5 +6,7 @@
 # python3 pacman.py -p AlphaBetaAgent -g DirectionalGhost -n 10 -q
 # python3 autograder.py -q part2
 
-python3 pacman.py -p ExpectimaxAgent -g DirectionalGhost -n 10 -q
+# python3 pacman.py -p ExpectimaxAgent -g DirectionalGhost -n 10 -q
 # python3 autograder.py -q part3
+
+python3 autograder.py -q part4  # --no-graphics
